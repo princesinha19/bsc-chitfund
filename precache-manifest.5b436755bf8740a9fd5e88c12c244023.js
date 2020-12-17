a2352089cb963b16e122d391f876fbdb77eaf6d5
Prince Sinha <sinhaprince013@gmail.com>
2020-12-17 13:58:46 +0530

    Updates

---
+++ precache-manifest.5b436755bf8740a9fd5e88c12c244023.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa72db7e5cc74b2a403a9625bca2f93d",
+    "url": "/bsc-chitfund/index.html"
+  },
+  {
+    "revision": "39678d68d9620d416f7b",
+    "url": "/bsc-chitfund/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "b06b22e4277afe0b0c9c",
+    "url": "/bsc-chitfund/static/css/main.fad53284.chunk.css"
+  },
+  {
+    "revision": "39678d68d9620d416f7b",
+    "url": "/bsc-chitfund/static/js/2.072d4af4.chunk.js"
+  },
+  {
+    "revision": "3faf3673a940fdf51b9f8699e5a5df2d",
+    "url": "/bsc-chitfund/static/js/2.072d4af4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b06b22e4277afe0b0c9c",
+    "url": "/bsc-chitfund/static/js/main.0cd134f7.chunk.js"
+  },
+  {
+    "revision": "37c2693868d0a9da7ee2",
+    "url": "/bsc-chitfund/static/js/runtime-main.88ff11a4.js"
+  },
+  {
+    "revision": "d8e9b0e1beb4a45811e0252f74e44b39",
+    "url": "/bsc-chitfund/static/media/bsc.d8e9b0e1.svg"
+  },
+  {
+    "revision": "b7f124e82ec36dae6a5582cc2a95e6b6",
+    "url": "/bsc-chitfund/static/media/logo.b7f124e8.png"
+  },
+  {
+    "revision": "e9ada72b93b9cdb343c1f49bfa413c29",
+    "url": "/bsc-chitfund/static/media/metamask.e9ada72b.png"
+  }
+]);
